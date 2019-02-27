57de73d815ae3ce17417bb2711f0ff1bac0c1536
Peter Squicciarini <stripedpajamas273@gmail.com>
2019-02-26 22:05:50 -0500

    Stub fetch for SSR

--- api/index.js
+++ api/index.js
@@ -1,7 +1,6 @@
-const fetch = window.fetch
-
 class API {
   constructor () {
+    this.fetch = global.window ? global.window.fetch : () => ({})
     this.host = 'co7096zvs2.execute-api.us-east-1.amazonaws.com'
     this.env = process.env.NODE_ENV !== 'production'
       ? 'test'
@@ -14,29 +13,29 @@ class API {
   login (email) {
     const endpoint = 'amaxon_login'
     const url = `${this.base}${endpoint}?email=${email}`
-    return fetch(url, this.defaultOpts)
+    return this.fetch(url, this.defaultOpts)
   }
   authenticate (email, token) {
     const endpoint = 'amaxon_authenticate'
     const url = `${this.base}${endpoint}?email=${email}&token=${token}`
-    return fetch(url, this.defaultOpts)
+    return this.fetch(url, this.defaultOpts)
   }
   checkAuth () {
     const endpoint = 'amaxon_check_auth'
     const url = `${this.base}${endpoint}`
-    return fetch(url, this.defaultOpts)
+    return this.fetch(url, this.defaultOpts)
   }
   getProducts () {
     const endpoint = 'amaxon_get_products'
     const url = `${this.base}${endpoint}`
-    return fetch(url, this.defaultOpts)
+    return this.fetch(url, this.defaultOpts)
       .then(res => res.ok && res.json())
   }
   setProducts (products) {
     const endpoint = 'amaxon_set_products'
     const url = `${this.base}${endpoint}`
     const body = JSON.stringify({ products })
-    return fetch(url, Object.assign({
+    return this.fetch(url, Object.assign({
       method: 'POST',
       body
     }, this.defaultOpts))
@@ -45,7 +44,7 @@ class API {
   signOut () {
     const endpoint = 'amaxon_sign_out'
     const url = `${this.base}${endpoint}`
-    return fetch(url, this.defaultOpts)
+    return this.fetch(url, this.defaultOpts)
   }
 }
 
